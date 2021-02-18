b6dbd694041b6e57246e629accb4db42fe42932e
SahilHemnani777 <hemnanisahil777@gmail.com>
2021-02-18 19:36:05 +0530

    completed calender view

--- app/src/main/java/com/example/shanksvilla/home/BookingActivity.java
+++ app/src/main/java/com/example/shanksvilla/home/BookingActivity.java
@@ -1,21 +1,31 @@
 package com.example.shanksvilla.home;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shanksvilla.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class BookingActivity extends AppCompatActivity {
     private static final String TAG = "BookingActivity";
     private Button btnSet;
     private CalendarView calender;
-    private TextView date;
+    private TextView dateFrom;
+    private TextView dateTo;
+    private int startDate, endDate;
+    private TextView displayText;
+    public int counter=0;
+    public         int x=0,y=0,z=0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +34,11 @@ public class BookingActivity extends AppCompatActivity {
 
         btnSet= findViewById(R.id.buttonSet);
         calender= findViewById(R.id.calendarView);
-        date= findViewById(R.id.date);
+        dateFrom= findViewById(R.id.datefrom);
+        dateTo= findViewById(R.id.dateto);
+        displayText= findViewById(R.id.textDisplay);
+
+        displayText.setText("Select the starting date");
 
         calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -32,9 +46,58 @@ public class BookingActivity extends AppCompatActivity {
                 // i2 -> day
                 // i1 -> month - 1
                 // i -> year
-                Log.d(TAG, "onSelectedDayChange: date"+ i2 +"-"+ (i1+1) +"-"+ i);
-                date.setText(i2 +"-"+ (i1+1) +"-"+ i);
+                if (counter==0){
+
+//                    Log.d(TAG, "onSelectedDayChange: date"+ i2 +"-"+ (i1+1) +"-"+ i);
+                    dateFrom.setText(i2 +"-"+ (i1+1) +"-"+ i);
+                    x=i2;
+                    y=i1;
+                    z=i;
+                    counter++;
+                    displayText.setText("Now select the end date");
+                }else{
+//                    Log.d(TAG, "onSelectedDayChange: "+x+y+z);
+                    if (i2>x && i1 >=y && i >=z){
+                        dateTo.setText(i2 +"-"+ (i1+1) +"-"+ i);
+                    }else{
+                        Toast.makeText(BookingActivity.this, "End date can't be before start date", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
+
+
+
+//        dateFrom.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startDate= calender.getDate();
+////                Log.d(TAG, "onClick: "+ getDate(date,"dd/MM/yyyy"));
+//
+//                dateFrom.setText(getDate(startDate,"dd/MM/yyyy"));
+//            }
+//        });
+//
+//        dateTo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                endDate=calender.getDate();
+//                dateTo.setText(getDate(endDate,"dd/MM/yyyy"));
+//            }
+//        });
+
+
+
+
+    }
+    public String getDate(long milliSeconds, String dateFormat)
+    {
+        // Create a DateFormatter object for displaying date in specified format.
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
     }
 }
