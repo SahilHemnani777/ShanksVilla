af219cfcd6ff8507ae0462f1dde703dcf94db301
SahilHemnani777 <hemnanisahil777@gmail.com>
2021-04-08 12:35:50 +0530

    booking

--- app/src/main/java/com/example/shanksvilla/home/booking/DetailsActivity.java
+++ app/src/main/java/com/example/shanksvilla/home/booking/DetailsActivity.java
@@ -25,56 +25,47 @@ public class DetailsActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
     private static final String TAG = "DetailsActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        myAuth= FirebaseAuth.getInstance();
-        FirebaseUser currentUser= myAuth.getCurrentUser();
-        name= findViewById(R.id.name_details);
-        age= findViewById(R.id.age_details);
-        phone= findViewById(R.id.contact_details);
-        btnDone= findViewById(R.id.done_details);
+        myAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = myAuth.getCurrentUser();
+        name = findViewById(R.id.name_details);
+        age = findViewById(R.id.age_details);
+        phone = findViewById(R.id.contact_details);
+        btnDone = findViewById(R.id.done_details);
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         Integer number = bundle.getInt("people");
         ArrayList<Integer> list = (ArrayList<Integer>) bundle.get("dates");
-        Log.d(TAG, "onCreate: "+ list+ "aa gayi");
-        if (bundle.getString("location").equals("kihim")){
-            btnDone.setOnClickListener(v -> {
-                assert currentUser != null;
-                String UID = currentUser.getUid();
-                String mName= name.getText().toString();
-                String mAge= age.getText().toString();
-                String mPhone= phone.getText().toString();
-                dbRef user = new dbRef(mName, Integer.parseInt(mAge), 0, mPhone, UID);
-                for (int i = 0; i<list.size(); i++){
-                    Log.d(TAG, "onClick: "+String.valueOf(list.get(i)));
-                    myRef.child("database1").child(String.valueOf(list.get(i))).child("bookings").setValue(user);
-                    myRef.child("database1").child(String.valueOf(list.get(i))).child("bookings").child("no_of_members").setValue(number);
-//                    myRef.child("database").child("vacancies")
+        Log.d(TAG, "onCreate: " + list + "aa gayi");
+
+        btnDone.setOnClickListener(v -> {
+            assert currentUser != null;
+            String UID = currentUser.getUid();
+            String mName = name.getText().toString();
+            String mAge = age.getText().toString();
+            String mPhone = phone.getText().toString();
+            dbRef user = new dbRef(mName, Integer.parseInt(mAge), 0, mPhone, UID);
+            if (bundle.getString("location").equals("kihim")) {
+                for (int i = 0; i < list.size(); i++) {
+                    Log.d(TAG, "onClick: " + list.get(i));
+                    myRef.child("database1").child(String.format("%08d",list.get(i))).child("bookings").setValue(user);
+                    myRef.child("database1").child(String.format("%08d",list.get(i))).child("bookings").child("no_of_members").setValue(number);
+                }
+            } else {
+                for (int i = 0; i < list.size(); i++) {
+                    Log.d(TAG, "onClick: " + list.get(i));
+                    myRef.child("database2").child(String.format("%08d",list.get(i))).child("bookings").setValue(user);
+                    myRef.child("database2").child(String.format("%08d",list.get(i))).child("bookings").child("no_of_members").setValue(number);
                 }
 
-            });
-        }else{
-            btnDone.setOnClickListener(v -> {
-                assert currentUser != null;
-                String UID = currentUser.getUid();
-                String mName= name.getText().toString();
-                String mAge= age.getText().toString();
-                String mPhone= phone.getText().toString();
-                dbRef user = new dbRef(mName, Integer.parseInt(mAge), 0, mPhone, UID);
-                for (int i = 0; i<list.size(); i++){
-                    Log.d(TAG, "onClick: "+String.valueOf(list.get(i)));
-                    myRef.child("database2").child(String.valueOf(list.get(i))).child("bookings").setValue(user);
-                    myRef.child("database2").child(String.valueOf(list.get(i))).child("bookings").child("no_of_members").setValue(number);
-//                    myRef.child("database").child("vacancies")
-                }
 
-            });
-        }
-
+            }
+        });
 
 
     }
