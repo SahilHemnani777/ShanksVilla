21f2b90c68cabc76dac767c53153f6b418adc50b
SahilHemnani777 <hemnanisahil777@gmail.com>
2021-04-16 19:38:35 +0530

    home'

--- app/src/main/java/com/example/shanksvilla/home/fragments/HomeFragment.java
+++ app/src/main/java/com/example/shanksvilla/home/fragments/HomeFragment.java
@@ -1,21 +1,40 @@
 package com.example.shanksvilla.home.fragments;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.shanksvilla.R;
+import com.example.shanksvilla.home.booking.BookingActivity;
 
 
 public class HomeFragment extends Fragment {
+
+    private Button locationButton, dateButton, viewDetailsButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        locationButton=v.findViewById(R.id.locationbtn);
+        dateButton= v.findViewById(R.id.datesbtn);
+        viewDetailsButton= v.findViewById(R.id.viewDetailsButton);
+
+        dateButton.setOnClickListener(v13 -> startActivity(new Intent(getActivity(), BookingActivity.class)));
+
+        locationButton.setOnClickListener(v12 -> new AlertDialog.Builder(getActivity())
+                .setTitle("Locations")
+                .setMessage("1. Kihim Beach\n2.Pune")
+                .setCancelable(true).show());
+
+        return v;
     }
 }
